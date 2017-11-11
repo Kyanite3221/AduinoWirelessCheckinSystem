15a13e0aad976453213d2d8a023426e23d0572f8
Wibren Wiersma <wibrenwiersma@hotmail.com>
2017-11-11 11:27:07 +0100

    Server restarts in case of network errors

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -20,48 +20,57 @@ namespace UDPServer {
         }
 
         static void Main(string[] args) {
-            UdpClient server = new UdpClient(new IPEndPoint(IPAddress.Any, 12345));
-            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            for (; ; ) {
+                try {
+                    using (UdpClient server = new UdpClient(new IPEndPoint(IPAddress.Any, 12345))) {
 
-            //database.LogRFID("123");
+                        IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
 
-            for (;;) {
-                byte[] Bdata = server.Receive(ref sender);
+                        //database.LogRFID("123");
 
-                CommandID command = (CommandID)Bdata[0];
+                        for (; ; ) {
+                            byte[] Bdata = server.Receive(ref sender);
 
-                switch (command) {
-                    case CommandID.RFIDRev:
-                        Console.WriteLine($"{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()}: {sender.Address.ToString()}: RFID RECIEVED");
+                            CommandID command = (CommandID)Bdata[0];
 
-                        //code for SQL group and log storage
-                        break;
-                    case CommandID.InfoReq:
-                        Console.WriteLine($"{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()}: {sender.Address.ToString()}: INFOREQ RECIEVED");
+                            switch (command) {
+                                case CommandID.RFIDRev:
+                                    Console.WriteLine($"{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()}: {sender.Address.ToString()}: RFID RECIEVED");
 
-                        //code for SQL group read
-                        //byte data = 0b1111_1111;
+                                    //code for SQL group and log storage
+                                    break;
+                                case CommandID.InfoReq:
+                                    Console.WriteLine($"{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()}: {sender.Address.ToString()}: INFOREQ RECIEVED");
 
-                        using (UdpClient client = new UdpClient()) {
-                            client.Connect(sender);
-                            client.Send(new byte[2] { (byte)CommandID.GroupSend, database.ReadGroups() }, 2);
-                            client.Close();
+                                    //code for SQL group read
+                                    //byte data = 0b1111_1111;
+
+                                    using (UdpClient client = new UdpClient()) {
+                                        client.Connect(sender);
+                                        client.Send(new byte[2] { (byte)CommandID.GroupSend, database.ReadGroups() }, 2);
+                                        client.Close();
+                                    }
+                                    break;
+                                case CommandID.Ping:
+                                    Console.WriteLine($"{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()}: {sender.Address.ToString()}: PING RECIEVED");
+
+                                    using (UdpClient client = new UdpClient()) {
+                                        client.Connect(sender);
+                                        client.Send(new byte[1] { (byte)CommandID.Ping }, 1);
+                                        client.Close();
+                                    }
+                                    break;
+                                case CommandID.Pong:
+                                    Console.WriteLine($"{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()}: {sender.Address.ToString()}: PONG RECIEVED");
+                                    break;
+                                default:
+                                    break;
+                            }
                         }
-                        break;
-                    case CommandID.Ping:
-                        Console.WriteLine($"{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()}: {sender.Address.ToString()}: PING RECIEVED");
-
-                        using (UdpClient client = new UdpClient()) {
-                            client.Connect(sender);
-                            client.Send(new byte[1] { (byte)CommandID.Ping }, 1);
-                            client.Close();
-                        }
-                        break;
-                    case CommandID.Pong:
-                        Console.WriteLine($"{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()}: {sender.Address.ToString()}: PONG RECIEVED");
-                        break;
-                    default:
-                        break;
+                    }
+                }catch(Exception ex) {
+                    Console.Error.WriteLine("Intern restart after: ");
+                    Console.Error.WriteLine($"{DateTime.Now.ToShortDateString()} {DateTime.Now.ToLongTimeString()}: {ex.GetType().FullName} {ex.Message}:\n{ex.StackTrace}");
                 }
             }
         }
